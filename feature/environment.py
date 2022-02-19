a3b91fa9cadef29116b41418ff1a3058d2b57293
Ruaney Mainarth <ruaney.mainarth@edu.ufes.br>
2022-02-19 19:42:16 -0300

    archive log and new scenario

--- feature/environment.py
+++ feature/environment.py
@@ -1,18 +1,7 @@
-from webdriver_manager.firefox import GeckoDriverManager
+
 from selenium import webdriver 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.firefox.options import Options
-
-
 def before_all(context):
-    firefoxOptions = Options()
-    firefoxOptions.add_argument("--headless")
-    context.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=firefoxOptions)
-    context.actions = ActionChains(context.driver)
-    context.wait = WebDriverWait(context.driver,15)
-    #context.driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    
-def before_scenario(context,scenario):
-    context.driver.maximize_window()
-    
+    context.webdriver = webdriver
+    pass
+def after_all(context):
+    pass
